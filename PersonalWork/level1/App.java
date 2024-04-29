e4b6c41b476a5421b19a13ec4257ba005ad3b9d4
berithx <seere7777@gmail.com>
2024-04-29 18:01:29 +0900

    leve1 / 1번 과제(양의 정수 2개 전달) 수정

--- PersonalWork/level1/App.java
+++ PersonalWork/level1/App.java
@@ -6,19 +6,40 @@ public class App {
     public static void main(String[] args) {
         int firstNumber = 0;
         int secondNumber = 0;
+        char operator;
 
         Scanner sc = new Scanner(System.in);
 
-        // 양수을 받고 다음 단계로 이동할 수 있도록 while 반복문으로 음수 거부
+        // 양수을 받고 다음 단계로 이동할 수 있도록 while 반복문과 if-else 조건문으로 음수 및 문자 입력 거부
         System.out.print("첫 번째 숫자를 입력해주세요 : ");
-        while ((firstNumber = sc.nextInt()) < 0) {
-            System.out.println("양의 정수(0 포함)를 입력해주세요.");
+        while (true) {
+            if (sc.hasNextInt()) {    // 입력값이 정수인지 아닌지 boolean 타입으로 출력
+                firstNumber = sc.nextInt();
+                if (firstNumber >= 0) {    // 입력된 값이 0 이상일 경우 break로 while문 탈출
+                    break;
+                } else {
+                    System.out.println("양의 정수(0 포함)만 입력해주세요.");    // 음수를 받은 경우 재입력 메세지 출력
+                }
+            } else {
+                System.out.println("양의 정수(0 포함)만 입력해주세요.");    // 정수 외 데이터를 받은 경우 재입력 메세지 출력
+                sc.next();    // 입력된 정수 외 데이터를 제거하여 무한루프 진입 방지
+            }
         }
 
         // 양수을 받고 다음 단계로 이동할 수 있도록 while 반복문으로 음수 거부
         System.out.print("두 번째 숫자를 입력해주세요 : ");
-        while ((secondNumber = sc.nextInt()) < 0) {
-            System.out.println("양의 정수(0 포함)를 입력해주세요.");
+        while (true) {
+            if (sc.hasNextInt()) {    // 입력값이 정수인지 아닌지 boolean 타입으로 출력
+                secondNumber = sc.nextInt();
+                if (secondNumber >= 0) {    // 입력된 값이 0 이상일 경우 break로 while문 탈출
+                    break;
+                } else {
+                    System.out.println("양의 정수(0 포함)만 입력해주세요.");    // 음수를 받은 경우 재입력 메세지 출력
+                }
+            } else {
+                System.out.println("양의 정수(0 포함)만 입력해주세요.");    // 정수 외 데이터를 받은 경우 재입력 메세지 출력
+                sc.next();    // 입력된 정수 외 데이터를 제거하여 무한루프 진입 방지
+            }
         }
 
         System.out.println("첫 번째 숫자 : " + firstNumber + ", 두 번째 숫자 : " + secondNumber);
