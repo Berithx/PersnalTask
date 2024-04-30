30a37082c9b231a32e53917751fc70ac44715f7b
berithx <seere7777@gmail.com>
2024-04-30 12:21:08 +0900

    leve2 / 1번 과제(매개변수를 통한 사칙연산 수행 후 결과값 반환 메서드, 결과값 저장 컬렉션 및 예외처리)

---
+++ PersonalWork/level2/BadInputException.java
@@ -0,0 +1,7 @@
+package 과제.PersonalWork.level2;
+
+public class BadInputException extends Exception {
+    public BadInputException(String type) {
+        super(type);
+    }
+}
---
+++ PersonalWork/level2/Calculator.java
@@ -0,0 +1,31 @@
+package 과제.PersonalWork.level2;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    ArrayList<Integer> resultArr = new ArrayList<>();    // 연산결과 저장 배열(순서 보장과 데이터 중복을 허용하는 컬렉션)
+
+    public Calculator() {
+    }
+
+    public int calculate (int firstNumber, char operator, int secondNumber) throws BadInputException {
+        int result = 0;
+
+        if(operator == '+') {
+            result = firstNumber + secondNumber;
+        } else if (operator == '-') {
+            result = firstNumber - secondNumber;
+        } else if (operator == '*') {
+            result = firstNumber * secondNumber;
+        } else if (operator == '/') {
+            if (secondNumber == 0) {
+                throw new BadInputException("나눗셈 분모에는 0이 입력될 수 없습니다.");
+            }
+            result = firstNumber / secondNumber;
+        } else {
+            throw new BadInputException("연산자에는 +, -, *, /만 입력할 수 있습니다.");
+        }
+
+        return result;
+    }
+}
