2fc5014e49dde15d3e7ace867f0b85904e1502c1
berithx <seere7777@gmail.com>
2024-04-30 14:46:33 +0900

    leve2 / 3번 과제(계산 결과가 저장되는 컬렉션 필드 private 접근 제어자 및 간접 접근 메서드 설정 후 활용)

--- PersonalWork/level2/App.java
+++ PersonalWork/level2/App.java
@@ -10,7 +10,8 @@ public class App {
 
         // Calculator 인스턴스 객체 생성
         Calculator calc = new Calculator();
-
+        
+        // 외부 입력(Scanner) 객체 생성
         Scanner sc = new Scanner(System.in);
 
         do {    // 1번의 계산이 끝난 후 exit 입력여부로 반복계산 실행
@@ -25,7 +26,8 @@ public class App {
             // 두 번째 숫자 입력
             System.out.print("두 번째 숫자를 입력해주세요 : ");
             secondNumber = sc.nextInt();
-
+            
+            // Calculator 객체로 넘어간 데이터 상에서 예외사항 발생 시 핸들링
             try {
             // 입력된 연산자, 피연산자를 Calculator 객체의 calculate 메소드 매개변수로 사용해서 호출 및 출력
             System.out.println("연산 결과 : " + calc.calculate(firstNumber, operator, secondNumber));
@@ -34,16 +36,16 @@ public class App {
             }
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            // remove 입력 시 Caculator 객체의 ArrayList 필드를 remove 메소스를 사용한 index 0번째 값 삭제
+            // remove 입력 시 Caculator 객체의 ArrayList 필드를 setter 메서드를 통해 간접 접근하여 index 0번째 값 삭제
             if (sc.next().equals("remove")) {
-                calc.resultArr.remove(0);
+                calc.delResultArr(0);    
                 System.out.println("삭제되었습니다.");
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            // inquiry 입력 시 향상된 for문을 통한 Calculator 객체의 ArrayList 필드에 저장된 데이터 전체 조회
+            // inquiry 입력 시 Calculator 객체의 ArrayList 필드에 저장된 데이터를 getter 메서드를 통해 간접 접근하여 읽기
             if (sc.next().equals("inquiry")) {
-                System.out.println(calc.resultArr.toString());
+                System.out.println(calc.getResult());
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
--- PersonalWork/level2/Calculator.java
+++ PersonalWork/level2/Calculator.java
@@ -3,7 +3,8 @@ package 과제.PersonalWork.level2;
 import java.util.ArrayList;
 
 public class Calculator {
-    ArrayList<Integer> resultArr = new ArrayList<>();    // 연산결과 저장 배열(순서 보장과 데이터 중복을 허용하는 컬렉션)
+    // private final 접근제어자 설정으로 외부에서 접근하는 것과 수정하는 것 방지
+    private final ArrayList<Integer> resultArr = new ArrayList<>();    // 연산결과 저장 배열(순서 보장과 데이터 중복을 허용하는 컬렉션)
 
     public Calculator() {
     }
@@ -29,5 +30,15 @@ public class Calculator {
         resultArr.add(result);
 
         return result;
+    }
+
+    // 외부에서 데이터를 읽을 수 있도록 getter 메서드 선언
+    public String getResult() {
+        return resultArr.toString();
+    }
+
+    // 외부에서 데이터를 수정할 수 있도록 setter 메서드 선언
+    public void delResultArr(int index) {
+        this.resultArr.remove(index);
     }
 }
